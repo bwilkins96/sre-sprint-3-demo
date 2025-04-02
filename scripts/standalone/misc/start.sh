6cacb7d119b7a4e367fc1c3b6ecafb275c80718c
bwilkins96 <ben.jr.speed@gmail.com>
2025-04-02 18:44:19 -0400

    fixed misspelling

--- scripts/standalone/misc/start.sh
+++ scripts/standalone/misc/start.sh
@@ -6,7 +6,7 @@ echo -e "BUILDING IMAGE: lc-image\n"
 
 docker build -t lc-image .
 
-echo -e "\nRUNNING CONTAINER: cl-container"
+echo -e "\nRUNNING CONTAINER: lc-container"
 echo "'latency-checker' or 'lc' is equivalent to 'python latency-checker.py'"
 echo -e "Run 'exit' to close the container\n"
 
